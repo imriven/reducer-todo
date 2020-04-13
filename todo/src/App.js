e6931415c3ebd7a15aa412ff674b476364ae7b95
Christina Harris <christinathedesigner@outlook.com>
2020-04-13 07:15:20 -0700

    finished converstion of todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import Styled from "styled-components"
+import './App.css'
+import Background from "./images/postit-scrabble-to-do-todo-3299.jpg"
+
+
+
+/****Styles****/
+
+const ImageBackground = Styled.div `
+display: flex;
+justify-content:center;
+background-image: url(${Background});
+background-size:cover;
+height: 100vh;
+`
+const ContainerDiv = Styled.div`
+display:flex;
+flex-direction: column;
+text-align: center;
+width: 80%;
+`
+
+const ExcellentHeader = Styled.h1 `
+font-family: 'Poppins', sans-serif;
+padding: 2%;
+margin-top: 30%;
+color: black;
+font-size: 1.5em;
+`
+
+/************/
+
+const initialValue = [
+  {
+    task: "Organize Garage",
+    id: uuidv4(),
+    completed: false,
+  },
+  {
+    task: "Bake Cookies",
+    id: uuidv4(),
+    completed: true,
+  },
+]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.payload]
+    case 'TOGGLE_COMPLETE':
+      return state.map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, completed: !item.completed };
+        } else {
+          return item;
+        }
+      })
+      case 'CLEAR_COMPLETE':
+        return state.filter(todo => !todo.completed )
+    default:
+      return state
+  }
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialValue)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ImageBackground>
+      <ContainerDiv>
+      <ExcellentHeader>Welcome To The TodoList App!</ExcellentHeader>
+      <TodoList
+        todos={state}
+        dispatch={dispatch}
+      />
+      <TodoForm dispatch={dispatch} />
+    </ContainerDiv>
+    </ImageBackground>
   );
 }
+
 
 export default App;
